c96e5220f10286d342383fc8f07507c445d4f4b6
fpmacedo <lipe_macedo@msn.com>
2020-05-30 17:02:53 -0300

    Finalizado o desafio 02 - Primeira entrega

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response=>{
+      setRepositories(response.data);
+    })
+  })
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories',{
+      title:'Filipe',
+      url: 'https://github.com/Rocketseat',
+      techs: ['Node','React']
+    })
+
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        
+        {repositories.map(repository=>(
+        <li key={repository.id}>
+          {repository.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
